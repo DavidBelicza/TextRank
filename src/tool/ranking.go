3f02b2482fd8576c33c1058e011d87cbb7cc4887
DavidBelicza <87.bdavid@gmail.com>
2018-01-13 18:13:15 +0100

    Ranking skeleton

---
+++ src/tool/ranking.go
@@ -0,0 +1,27 @@
+package tool
+
+func Rank(sentence ParsedSentence) {
+	for _, word := range sentence.GetWords() {
+		updateGraph(word)
+	}
+}
+
+func updateGraph(word string) {
+
+}
+
+func isWordExist() {
+
+}
+
+func addSentence() {
+
+}
+
+func addWord() {
+
+}
+
+func updateRanks() {
+
+}
