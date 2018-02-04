c54cd2975845e8b67048a28b621652738581e177
DavidBelicza <87.bdavid@gmail.com>
2018-02-04 16:25:11 +0100

    Travis CI

--- parse/tokenizer_test.go
+++ parse/tokenizer_test.go
@@ -13,8 +13,8 @@ func TestParse(t *testing.T) {
 	assert.Equal(t, 2, len(parsedText.GetSentences()[0].GetWords()), "Word count")
 	assert.Equal(t, 5, len(parsedText.GetSentences()[1].GetWords()), "Word count")
 
-	assert.Equal(t, "Hello", parsedText.GetSentences()[0].GetWords()[0])
-	assert.Equal(t, "Now", parsedText.GetSentences()[1].GetWords()[0])
+	assert.Equal(t, "hello", parsedText.GetSentences()[0].GetWords()[0])
+	assert.Equal(t, "now", parsedText.GetSentences()[1].GetWords()[0])
 	assert.Equal(t, "sentence", parsedText.GetSentences()[1].GetWords()[4])
 
 	parsedText2 := TokenizeText("This is a sentence without mark")
